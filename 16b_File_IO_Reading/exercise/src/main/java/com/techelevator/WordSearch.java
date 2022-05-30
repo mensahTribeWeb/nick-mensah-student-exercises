de329806968bfa35dd3435c254360222fc80d0f8
mensahTribeWeb <ndm.acct85@gmail.com>
2022-05-30 10:53:28 -0500

    Completion of IO read 1

--- 16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ 16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -5,46 +5,46 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class WordSearch {
-	public static void main(String[] args) throws FileNotFoundException,Exception {
+    public static void main(String[] args) throws FileNotFoundException, Exception {
         Scanner scan = new Scanner(System.in);
-        System.out.println("What is the fully qualified name of the file that should be searched?\n"+
+        System.out.println("What is the fully qualified name of the file that should be searched?\n" +
                 "[path-to-the-file]: ");
         File searchedFile = new File(scan.nextLine());
 
         System.out.println("What is the search word you are looking for?");
-        String wordSearched = ""+ scan.nextLine()+"";
+        String wordSearched = "" + scan.nextLine() + "";
 
 
         System.out.println("Should the search be case sensitive? (Y\\N)");
         String answer = scan.nextLine();
         boolean isCaseSensitive = answer.equalsIgnoreCase("y");
-try {
-    Scanner userInput = new Scanner(searchedFile);
-    int lineNumber = 1;
+        try {
+            Scanner userInput = new Scanner(searchedFile);
+            int lineNumber = 1;
+            while (userInput.hasNextLine()) {
+                if (isCaseSensitive) {
 
-    if(isCaseSensitive) {
-        while (userInput.hasNextLine()){
-            String lineVerification = userInput.nextLine();
-            lineNumber++;
-            if(lineVerification.contains(wordSearched)){
-                System.out.println(lineNumber+")"+" "+lineVerification);
+                    String lineVerification = userInput.nextLine();
+                    lineNumber++;
+                    if (lineVerification.contains(wordSearched)) {
+                        System.out.println(lineNumber + ")" + " " + lineVerification);
+                    }
+                } else if (answer.equalsIgnoreCase("n")) {
+                    lineNumber = 1;
+                    while (userInput.hasNextLine()) {
+                        String lineVerification = userInput.nextLine();
+                        lineNumber++;
+                        if (lineVerification.toLowerCase().contains(wordSearched.toLowerCase())) {
+                            System.out.println(lineNumber + ")" + " " + lineVerification);
+                        }
+                    }
+                }
+
             }
-        }
-    }
-    else if(answer.equalsIgnoreCase("n")){
-         lineNumber = 1;
-        while (userInput.hasNextLine()){
-            String lineVerification = userInput.nextLine();
-            lineNumber++;
-            if(lineVerification.toLowerCase().contains(wordSearched.toLowerCase())){
-              System.out.println(lineNumber +")"+" "+lineVerification);
-          }
-        }
-    }
 
-} catch (Exception e) {
-    e.printStackTrace();
-}
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
 //test
