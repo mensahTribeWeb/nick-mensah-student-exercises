05812e1632f13c77619d5f61af3fc08a07eba69f
mensahTribeWeb <ndm.acct85@gmail.com>
2022-06-27 02:02:28 -0500

    Exercise grouping, ording, limiting COMPLETION

--- 02_Ordering_Limiting_and_Grouping/exercise/Exercises/19_states_per_census_region.sql
+++ 02_Ordering_Limiting_and_Grouping/exercise/Exercises/19_states_per_census_region.sql
@@ -1,5 +1,11 @@
--- 19. The census region and the number of records in the state table (name column 'num_states') for each census region. Exclude ones that don't have a census region.
+-- 19. The census region and the number of records in the state table (name column 'num_states') 
+--for each census region. Exclude ones that don't have a census region.
 -- Order the results from highest to lowest.
 -- (Note: DC is in a census region, but the territories aren't, so the sum of the counts will equal 51).
 -- (4 rows)
+SELECT COUNT(state_abbreviation) ||' '|| census_region AS num_states
+FROM state
+WHERE census_region IS NOT NULL
+GROUP BY census_region
+ORDER BY census_region DESC;
 
