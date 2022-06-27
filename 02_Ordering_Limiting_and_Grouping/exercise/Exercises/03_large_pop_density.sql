aef81b798da8e6a7315bb58ec97be911af684bb3
mensahTribeWeb <ndm.acct85@gmail.com>
2022-06-26 22:48:52 -0500

    Exercise grouping, ording, limiting UPDATE

--- 02_Ordering_Limiting_and_Grouping/exercise/Exercises/03_large_pop_density.sql
+++ 02_Ordering_Limiting_and_Grouping/exercise/Exercises/03_large_pop_density.sql
@@ -1,5 +1,9 @@
--- 3. The name, population, area, and population density (name the column 'population_density') of cities with more than 5,000 people per square kilometer.
--- Population density is expressed as people per square kilometer. In other words, population divided by area.
+-- 3. The name, population, area, and population density (name the column 'population_density') of cities 
+--with more than 5,000 people per square kilometer.
+-- Population density is expressed as people per square kilometer. In other words, population divided by 
+--area.
 -- Order the results by population density, highest number first.
 -- (9 rows)
-
+SELECT 
+FROM city
+ORDER BY population_density DESC;
